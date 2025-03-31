1fc332b58ba8062bc4be7d18023c9d3e97bd321a
Mykyta Grogul <mykyta006@gmail.com>
2025-03-31 20:26:08 +0200

    Fixade gui

--- Trading Simulation Platform/src/Main.java
+++ Trading Simulation Platform/src/Main.java
@@ -1,8 +1,24 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+import java.io.*;
+import javax.swing.*;
+import java.awt.*;
 public class Main {
     public static void main(String[] args) {
-        
+
+        JFrame frame = new JFrame("Stock Simulator");
+        frame.setSize(1000, 600);
+        frame.setLayout(null);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setVisible(true);
+
+        JLabel welcomeText = new JLabel("Welcome to Stock Simulator", SwingConstants.CENTER);
+        welcomeText.setFont(new Font("Arial", Font.BOLD, 40)); // Fix: Doubled text size
+        welcomeText.setBounds((1000 - 600) / 2, 50, 600, 100); // Fix: Increased width to fit text
+        frame.add(welcomeText);
+
+
+
+
+
 
     }
 }
